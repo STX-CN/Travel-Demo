d2c3446a18fbc16f23be70d12640e81164016df4
zhujm <zhujm@smartcloud-china.com>
2018-05-16 08:23:19 +0800

    初始化项目

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+  <div class="home">Home Page</div>
+  <router-link to="/list">go to list</router-link>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+<style>
+  .home{
+    font-size: 50px;
+  }
+</style>
---
+++ src/pages/list/List.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="list">List Page</div>
+</template>
+
+<script>
+export default {
+  name: 'List'
+}
+</script>
+<style>
+  .list{
+    font-size: 50px;
+  }
+</style>
