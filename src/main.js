3d4decb074e09678e8d48afe122461d4b978db7a
zhujm <zhujm@smartcloud-china.com>
2018-05-16 08:44:17 +0800

    1. 添加reset.css
    2. 添加border,解决移动端1px的问题
    3. 安装fastClick，解决移动端300ms延迟响应的问题
       $: npm install fastclick --save

--- src/main.js
+++ src/main.js
@@ -3,8 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// 解决移动端点击事件300ms延迟的问题
+import fastClick from 'fastclick'
+import './assets/styles/reset.css'
+// 解决移动端1px的问题
+import './assets/styles/border.css'
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
