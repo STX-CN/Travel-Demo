a172db9f3774b702e191dc6552aaa3139f0ac885
zhujm <zhujm@smartcloud-china.com>
2018-05-16 08:46:55 +0800

    去除掉不必要的代码

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,7 +1,6 @@
 <template>
 <div>
-  <div class="home">Home Page</div>
-  <router-link to="/list">go to list</router-link>
+  HelloWorld
 </div>
 </template>
 
@@ -11,7 +10,4 @@ export default {
 }
 </script>
 <style>
-  .home{
-    font-size: 50px;
-  }
 </style>
