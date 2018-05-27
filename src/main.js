b16b9c278bcfc87b5579a147b1ddf9e0f3b21ffd
zhujm <zhujm@smartcloud-china.com>
2018-05-27 16:18:14 +0800

    增加轮播效果

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import App from './App'
 import router from './router'
+import 'swiper/dist/css/swiper.css'
 // 解决移动端点击事件300ms延迟的问题
 import fastClick from 'fastclick'
 import './assets/styles/reset.css'
@@ -12,6 +14,7 @@ import './assets/styles/iconfont.css'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper /* { default global options } */)
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="wrapper">
+    <swiper  :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl"/>
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data: function () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [
+        {
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1805/e5/59fad13a64807d02.jpg_750x200_713ae984.jpg'
+        },
+        {
+          id: '0002',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1805/cb/2110021e0365b702.jpg_750x200_05a70d80.jpg'
+        },
+        {
+          id: '0003',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1805/10/3ee6df72beb86402.jpg_750x200_e02a4c99.jpg'
+        }
+
+      ]
+    }
+  }
+}
+</script>
+
+<style lang='stylus' scoped>
+  .wrapper >>>  .swiper-pagination-bullet-active
+      background : #fff
+  .wrapper
+    width: 100%
+    height : 31.25%vw
+    background : #eee
+    .swiper-img
+      width:100%
+</style>
