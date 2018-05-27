4466df059bc952d3a6617af4bf8e64736cedcc34
zhujm <zhujm@smartcloud-china.com>
2018-05-27 22:36:24 +0800

    添加周末去哪儿的页面

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,9 +1,14 @@
 <template>
-<div class="header">
-  <div class="header-left">返回</div>
-  <div class="header-input">输入城市/景点/游玩主题</div>
-  <div class="header-right">城市</div>
-</div>
+  <div class="header">
+    <div class="header-left">
+      <div class="iconfont back-icon">返回</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">城市</div>
+  </div>
 </template>
 
 <script>
@@ -11,26 +16,37 @@ export default {
   name: 'HomeHeader'
 }
 </script>
+
 <style lang="stylus" scoped>
+  @import '../../../assets/styles/mixins.styl'
   .header
-    display : flex
-    line-height : .86rem
-    background : #00bcd4
-    color : #fff
+    display: flex
+    line-height: $headerHeight
+    background: $bgColor
+    color: #fff
     .header-left
-      width: 0.64rem
+      width: .64rem
       float: left
+      .back-icon
+        text-align: center
+        font-size: .4rem
     .header-input
       flex: 1
-      height : .64rem
-      line-height : .64rem
-      margin-top :.12rem
-      background :#fff
-      border-radius :.1rem
-      margin-left :.2rem
-      color #ccc
+      height: .64rem
+      line-height: .64rem
+      margin-top: .12rem
+      margin-left: .2rem
+      padding-left: .2rem
+      background: #fff
+      border-radius: .1rem
+      color: #ccc
     .header-right
-      width : 1.24rem
+      min-width: 1.04rem
+      padding: 0 .1rem
       float: right
-      text-align :center
+      text-align: center
+      color: #fff
+      .arrow-icon
+        margin-left: -.04rem
+        font-size: .24rem
 </style>
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,72 @@
+<template>
+<div>
+  <div class="title">周末去哪儿</div>
+  <ul>
+    <li class="item boder-bottom" v-for="item of recommendList" :key="item.id">
+      <div class="item-img-wrapper">
+        <img class="item-img" :src="item.imgUrl"/>
+      </div>
+      <div class="item-info">
+        <p class="item-title">{{item.title}}</p>
+        <p class="item-desc">{{item.desc}}</p>
+      </div>
+    </li>
+  </ul>
+</div>
+</template>
+
+<script>
+export default{
+  name: 'HomeWeekend',
+  data: function () {
+    return {
+      recommendList: [
+        {
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/sight/source/1505/68/de862f94e383a6.jpg_r_640x214_f9df927b.jpg',
+          title: '福州必游Top10',
+          desc: '大美福州, 必游之处'
+        },
+        {
+          id: '0002',
+          imgUrl: 'http://img1.qunarzz.com/sight/source/1509/b5/07bc81fc254ed6.jpg_r_640x214_8879c815.jpg',
+          title: '土楼不能不说的秘密',
+          desc: '一座座神秘的建筑，一段段传奇的历史'
+        },
+        {
+          id: '0003',
+          imgUrl: 'http://img1.qunarzz.com/sight/source/1505/f6/82acfa5c7f472c.jpg_r_640x214_508da3cb.jpg',
+          title: '熊孩子玩乐去哪儿',
+          desc: '这里向熊孩子们招手'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang=stylus scoped>
+  @import '../../../assets/styles/mixins.styl'
+  .title
+    margin-top: .2rem
+    line-height: .8rem
+    background: #eee
+    text-indent: .2rem
+    .item-img-wrapper
+      overflow: hidden
+      height : 0
+      padding-bottom : 33.9%
+      .item-img
+        width : 100%
+    .item-info
+      padding : .1rem
+      .item-title
+        line-height : .54rem
+        font-size: .32rem
+        ellipsis()
+      .item-desc
+        line-height : .4rem
+        font-size: .28rem
+        color : #ccc
+        ellipsis()
+</style>
